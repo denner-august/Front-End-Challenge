48c8d18edcf781e6180134fb2dcc69b4570ac2eb
Denner augusto De Paula Bernardes <41222616+denner-august@users.noreply.github.com>
2022-04-11 22:48:24 -0300

    Update index.tsx

--- coodash/src/components/Content/index.tsx
+++ coodash/src/components/Content/index.tsx
@@ -1,7 +1,24 @@
+import styles from "./styles.module.scss";
+import { Search } from "../search/index";
+import { Dispatch, SetStateAction, useState } from "react";
+import { TableNames } from "../table";
+
 export function Content() {
+  const [search, setSearch] =
+    useState<Dispatch<SetStateAction<string>>>(String);
+
   return (
     <>
-      <p>content</p>
+      <p className={styles.texto}>
+        A empresa Pharma Inc, está trabalhando em um projeto em colaboração com
+        sua base de clientes para facilitar a gestão e visualização da
+        informação dos seus pacientes de maneira simples e objetiva.
+      </p>
+
+      <Search searching={setSearch} />
+      <main className={styles.conteudo}>
+        <TableNames />
+      </main>
     </>
   );
 }
