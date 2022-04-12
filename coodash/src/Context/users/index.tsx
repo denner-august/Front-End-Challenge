d45eabbef21f68500c8d8d0ad808dc4a9f21f6e4
Denner augusto De Paula Bernardes <41222616+denner-august@users.noreply.github.com>
2022-04-12 12:49:20 -0300

    Update index.tsx

--- coodash/src/Context/users/index.tsx
+++ coodash/src/Context/users/index.tsx
@@ -1,27 +1,44 @@
+import useSWR from "swr";
+import {
+  createContext,
+  Dispatch,
+  ReactChild,
+  SetStateAction,
+  useEffect,
+  useState,
+} from "react";
 import axios from "axios";
-import { createContext, ReactChild, useEffect, useState } from "react";
 
 interface ContextProps {
   data:
     | {
         name: string;
         gender: string;
+        map?: any;
+        id: { value: string };
       }
-    | any;
+    | undefined;
+
+  setDatafetch?: Dispatch<SetStateAction<undefined>>;
 }
 
 const Context = createContext({} as ContextProps);
 
 export const ConxtProvider = (props: { children: ReactChild }) => {
-  const [data, setData] = useState<ContextProps[]>();
+  const [datafetch, setDatafetch] = useState([]);
 
-  useEffect(() => {
+  const fetcher = (url: string) =>
     axios
-      .get("https://randomuser.me/api/?page=1&results=1&seed=abc")
-      .then((response) => response.data)
-      .then((response) => response.results)
-      .then((response) => setData(response));
-  }, []);
+      .get(url)
+      .then((res) => res.data)
+      .then((res) => res.results);
+
+  const { data, error } = useSWR(
+    "https://randomuser.me/api/?page=1&results=10&seed=abc",
+    fetcher
+  );
+
+  // setDatafetch((preve) => (preve = data));
 
   return <Context.Provider value={{ data }}>{props.children}</Context.Provider>;
 };
