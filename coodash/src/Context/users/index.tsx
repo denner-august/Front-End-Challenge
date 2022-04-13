0d31a7da33d2d39a3880f074361884cc9b600247
Denner augusto De Paula Bernardes <41222616+denner-august@users.noreply.github.com>
2022-04-12 21:26:48 -0300

    Update index.tsx

--- coodash/src/Context/users/index.tsx
+++ coodash/src/Context/users/index.tsx
@@ -8,38 +8,33 @@ import {
   useState,
 } from "react";
 import axios from "axios";
+import axiosData from "../../services/axiosData";
+import { useFetch } from "../../hooks/useFetch";
 
 export interface ContextProps {
-  data: {
-    name: string;
-    gender: string;
-    map: any;
-    id: { value: string };
-    filter: any;
-  };
-
+  dataGlobal: never[];
   search: string;
   setSearch: Dispatch<SetStateAction<string>>;
+  setDataGlobal: (value: any) => void;
 }
 
 const Context = createContext({} as ContextProps);
 
 export const ConxtProvider = (props: { children: ReactChild }) => {
   const [search, setSearch] = useState("");
-
-  const fetcher = (url: string) =>
-    axios
-      .get(url)
-      .then((res) => res.data)
-      .then((res) => res.results);
-
-  const { data, error } = useSWR(
-    "https://randomuser.me/api/?page=1&results=10&seed=abc",
-    fetcher
+  const [dataGlobal, setDataGlobal] = useState<never[]>([]);
+  const { data, error } = useFetch(
+    "https://randomuser.me/api/?page=1&results=10&seed=abc"
   );
 
+  useEffect(() => {
+    if (data) {
+      setDataGlobal(data);
+    }
+  }, [data]);
+
   return (
-    <Context.Provider value={{ data, search, setSearch }}>
+    <Context.Provider value={{ dataGlobal, setDataGlobal, search, setSearch }}>
       {props.children}
     </Context.Provider>
   );
