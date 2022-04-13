520e54a6b46abc53dae68352356fa22a7cd65a88
Denner augusto De Paula Bernardes <41222616+denner-august@users.noreply.github.com>
2022-04-12 21:26:40 -0300

    Update index.tsx

--- coodash/src/pages/index.tsx
+++ coodash/src/pages/index.tsx
@@ -1,12 +1,14 @@
 import type { NextPage } from "next";
 import { Header } from "../components/header/index";
 import { Content } from "../components/Content/index";
+import { Button } from "../components/button";
 
 const Home: NextPage = () => {
   return (
     <div>
       <Header />
       <Content />
+      <Button />
     </div>
   );
 };
