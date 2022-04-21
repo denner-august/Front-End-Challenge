a725a2b0b82613e37c85696dbd42e587bed9c474
Denner augusto De Paula Bernardes <41222616+denner-august@users.noreply.github.com>
2022-04-21 17:44:29 -0300

    Update index.tsx

--- coodash/src/components/modal/index.tsx
+++ coodash/src/components/modal/index.tsx
@@ -1,37 +1,59 @@
-import { useState, useMemo } from "react";
+import { useState, useMemo, useEffect } from "react";
 import {
   useDisclosure,
   Button,
   Modal,
   ModalOverlay,
   ModalContent,
-  ModalHeader,
   ModalCloseButton,
   ModalBody,
   ModalFooter,
 } from "@chakra-ui/react";
+import { DadosPaciente } from "../pacienteficha";
 import styles from "./styles.module.scss";
+import { useFetch } from "../../hooks/useFetch";
+import { useFetchUrl } from "../../hooks/useFecthUrl";
 
 export function PacienteFicha({
   pacienteUrl,
   modalopen,
   closeModal,
   paciente,
+  dataPacientes,
 }: any) {
   const { onClose } = useDisclosure();
   const [urlPaciente, setUrlPaciente] = useState("qualquer coisa");
+  const [pacienteData, setPacienteData] = useState(null);
+
+  const { data, error } = useFetchUrl(
+    `https://randomuser.me/api/?page=1&results=1`
+  );
 
   const memoizedValue = useMemo(() => paciente, [paciente]); // como o use memo funciona
-  console.log(memoizedValue);
+
+  useEffect(() => {
+    if (data !== undefined && pacienteUrl) {
+      setPacienteData(data.results[0]);
+    }
+  }, [data]);
+
+  useEffect(() => {
+    setPacienteData(memoizedValue ?? memoizedValue);
+  }, [paciente]);
+
+  useEffect(() => {
+    setUrlPaciente(pacienteUrl);
+  }, [pacienteUrl]);
 
   return (
-    <>
+    <div>
       <Modal closeOnOverlayClick={false} isOpen={modalopen} onClose={onClose}>
         <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Ficha do paciente</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody pb={6}>{pacienteUrl ? pacienteUrl : "dados"}</ModalBody>
+        <ModalContent marginTop={"25vh"} width={"auto"} maxWidth={9000}>
+          <ModalCloseButton onClick={() => closeModal(false)} />
+          <ModalBody pb={6}>
+            <DadosPaciente dados={pacienteData} />
+          </ModalBody>
 
           <ModalFooter>
             <Button
@@ -45,6 +67,6 @@ export function PacienteFicha({
           </ModalFooter>
         </ModalContent>
       </Modal>
-    </>
+    </div>
   );
 }
