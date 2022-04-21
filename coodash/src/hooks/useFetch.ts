8575d0896822673b0cd4792bfdaed90f7ba69f8d
Denner augusto De Paula Bernardes <41222616+denner-august@users.noreply.github.com>
2022-04-21 17:44:02 -0300

    Update useFetch.ts

--- coodash/src/hooks/useFetch.ts
+++ coodash/src/hooks/useFetch.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import useSWR from "swr";
 
 export function useFetch(url:string){
-    const fetcher = (url: string) => axios.get(url).then((res) => res.data).then((res) => res.results);
+    const fetcher = (url: string) => axios.get(url).then((res) => res.data)
 
     const {data, error, } = useSWR(url, fetcher);
 
