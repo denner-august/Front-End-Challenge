a74b8e97747b7aa3e6763528d156b5e7e0de960f
Denner augusto De Paula Bernardes <41222616+denner-august@users.noreply.github.com>
2022-04-21 17:44:32 -0300

    Update index.tsx

--- coodash/src/components/button/index.tsx
+++ coodash/src/components/button/index.tsx
@@ -1,23 +1,23 @@
 import styles from "./styles.module.scss";
 import { useContext, useState } from "react";
-import useSWR from "swr";
-import axiosData from "../../services/axiosData";
-import { useEffect } from "react";
 import { useFetch } from "../../hooks/useFetch";
 import Context from "../../Context/users";
-import { ContextProps } from "../../Context/users/index";
 export function Button() {
   const [pageIndex, setPageIndex] = useState(2);
 
   const { dataGlobal, setDataGlobal } = useContext(Context);
   const { data, error } = useFetch(
-    `https://randomuser.me/api/?page=${pageIndex}&results=10&seed=abc`
+    `https://randomuser.me/api/?page=${pageIndex}&results=50&seed=1`
   );
 
   function newPages() {
     setPageIndex(pageIndex + 1);
     if (data) {
-      setDataGlobal([...dataGlobal, ...data]);
+      let FetchData = data.results.map((item: any) => item);
+      let GlobalData = dataGlobal.results.map((item: any) => item);
+      let FetchGlobalData = GlobalData.concat(FetchData);
+      const final = { results: [...FetchGlobalData], info: data.info };
+      setDataGlobal(final);
     }
   }
 
